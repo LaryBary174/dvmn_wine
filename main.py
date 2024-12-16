e81d4b9c49d783019c220a58ad6c2616fd190e4d
LaryBary174 <lotuc74@gmail.com>
2024-12-16 17:33:33 +0500

    исправления замечаний : удалены лишние файлы из репозитория дополнена инструкция по установке и запуску

--- main.py
+++ main.py
@@ -1,25 +1,40 @@
+import argparse
+
 from utils import get_year_word, categorize_wine, get_age_of_company
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
 
-template = env.get_template('template.html')
 
-age = get_age_of_company()
-wines = categorize_wine('wine3.xlsx')
-rendered_page = template.render(
-    age=age,
-    word_form=get_year_word(age),
-    wines=wines
-)
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--filename', help='Путь к Excel файлу')
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+    return parser
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+def main():
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+
+    template = env.get_template('template.html')
+    args = create_parser().parse_args()
+    excel_wine = args.filename
+    age = get_age_of_company()
+    wines = categorize_wine(excel_wine)
+    rendered_page = template.render(
+        age=age,
+        word_form=get_year_word(age),
+        wines=wines
+    )
+
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
+
+if __name__ == '__main__':
+    main()
